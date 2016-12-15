a7cb7f7c77a41241fee5c187cd95de869f8705b3
ZacJocelyn <zacjocelyn@gmail.com>
2016-12-14 22:09:29 -0700

    salt

--- routes/index.js
+++ routes/index.js
@@ -23,34 +23,17 @@ router.get('/new', (req, res, next)=>{
   res.render('new')
 })
 
-function validBand(band) {
-  return typeof band.name == 'string' &&
-          band.name.trim() != '' &&
-          typeof band.genre == 'string' &&
-          band.genre.trim() != '' &&
-          typeof band.number_of_albums == 'number' &&
-          typeof band.fav_song == 'string' &&
-          band.fav_song.trim() != ''
-}
-
 router.post('/', function (req, res, next){
-  if (validBand(req.body)) {
-    knex('favband').insert({
-        name: req.body.name,
-        genre: req.body.genre,
-        number_of_albums: req.body.number_of_albums,
-        fav_song: req.body.fav_song
-    }).returning('id').then(function (id){
-        res.redirect('/bands/' + id);
-    });
- } else {
-   res.status(500)
-   res.render('error',{
-     message: 'not a valid band'
-   })
- }
+   knex('favband').insert({
+       name: req.body.name,
+       genre: req.body.genre,
+       number_of_albums: req.body.number_of_albums,
+       fav_song: req.body.fav_song
+   }).returning('id').then(function (id){
+     console.log(id);
+       res.redirect('/bands/' + id);
+   });
 });
-
 router.delete('/bands/:id', (req, res, next) =>{
   knex('favband').where('id', req.params.id).del().then((bands)=>{
     res.redirect('/')
@@ -64,22 +47,14 @@ router.get('/bands/edit/:id', (req, res, next) =>{
   })
 })
 router.put('/bands/edit/:id', (req, res) =>{
-  if (validBand(req.body)) {
-    knex('favband').where('id', req.params.id).update({
-        name: req.body.name,
-        genre: req.body.genre,
-        number_of_albums: req.body.number_of_albums,
-        fav_song: req.body.fav_song
-    }).returning('id').then((id)=>{
-      console.log(id)
-      res.redirect('/bands/' + id)
-    })
-  }
-  else {
-   res.status(500)
-   res.render('error',{
-     message: 'not a valid band'
-   })
- }
+  knex('favband').where('id', req.params.id).update({
+      name: req.body.name,
+      genre: req.body.genre,
+      number_of_albums: req.body.number_of_albums,
+      fav_song: req.body.fav_song
+  }).returning('id').then((id)=>{
+    console.log(id)
+    res.redirect('/bands/' + id)
+  })
 })
 module.exports = router;
